c92b2e2663a5b6c76002ba1dc88a073407a8b712
IchildYu <1641727377@qq.com>
2023-08-29 23:48:08 +0800

    Update load_elf.h

--- include/load_elf.h
+++ include/load_elf.h
@@ -5,4 +5,6 @@ void* load_elf(const char* elf_path);
 void* get_symbol_by_name(void* base, const char* symbol);
 void* get_symbol_by_offset(void* base, size_t offset);
 
+extern int (*init_array_filter)(void* base, void (*init_array_item)());
+
 #endif
